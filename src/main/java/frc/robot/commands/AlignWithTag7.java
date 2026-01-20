b275dde158b6ec98381239a7be38a3736545beac
Noah Zemlin <noah.zemlin@gmail.com>
2026-01-20 16:34:30 -0600

    Completed alignment example

--- src/main/java/frc/robot/commands/AlignWithTag7.java
+++ src/main/java/frc/robot/commands/AlignWithTag7.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class AlignWithTag7  extends Command{
@@ -15,12 +16,30 @@ public class AlignWithTag7  extends Command{
 
     }
 
+    public double clamp(double min, double max, double value) {
+        if (value < min) {
+            return min;
+        }
+        if (value > max) {
+            return max;
+        }
+        return value;
+    }
+
 
     @Override
     public void execute() {
-        swerve.driveFieldOriented(new ChassisSpeeds(0.4,0.2,0));
-        
-        
+        double distFromTag7X = swerve.getPose().getTranslation().getX() - VisionSubsystem.fieldLayout.getTagPose(7).get().getX();
+        double distFromTag7Y = swerve.getPose().getTranslation().getY() - VisionSubsystem.fieldLayout.getTagPose(7).get().getY();
+        double angleFromTag7 = swerve.getPose().getRotation().getDegrees() - 0.0; // 0.0 is the desired angle
+
+        double xSpeed = -distFromTag7X * 5.0;
+        double ySpeed = -distFromTag7Y * 5.0;
+
+        xSpeed = clamp(-0.8, 0.8, xSpeed);
+        ySpeed = clamp(-0.8, 0.8, ySpeed);
+
+        swerve.driveFieldOriented(new ChassisSpeeds(xSpeed, ySpeed,0));
     }
     
 }
