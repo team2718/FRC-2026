2636541da7ef8242fca0f836ddafdd70fccc9532
Caleb Winterscheidt <iamnotallowedtotellyou@gmail.com>
2026-01-29 16:57:57 -0600

    Worked on Extend and Retract climber commands

---
+++ src/main/java/frc/robot/commands/climber/RetractHook.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.climber;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.ClimberSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RetractHook extends Command {
+    private ClimberSubsystem climber;
+
+    @Override
+    public void initialize() {
+        climber.setDesiredLevel(0);
+    }
+
+    @Override
+    public void execute() {
+        climber.setClimbMotor(climber.getHookElevation() * -1 * Constants.ClimberConstants.CLIMBER_P);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (climber.getHookElevation() <= 0.1);
+    }
+}
