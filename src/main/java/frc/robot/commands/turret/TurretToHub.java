ea15e2f0d4d282c05b5406e6471b9d9d0e6602aa
Zachary <150299755+ZacharyKingery@users.noreply.github.com>
2026-02-02 18:46:36 -0600

    Changed "diff > 180" to "dif >= 180" in TurretToHub Command

--- src/main/java/frc/robot/commands/turret/TurretToHub.java
+++ src/main/java/frc/robot/commands/turret/TurretToHub.java
@@ -41,7 +41,7 @@ public class TurretToHub extends Command {
     public static double getWrappedAngleDifference(double source, double target) {
         double diff = (target - source) % 360;
 
-        if (diff > 180) {
+        if (diff >= 180) {
             diff -= 360;
         }
         else if (diff <= -180) {
