2d1039cb315d61de4f00bd360efd7f2f98c7843e
ZacharyKingery <zdkingery@gmail.com>
2026-02-05 19:38:47 -0600

    Made initial calculations to account for shooting fuel while the robot is moving

--- src/main/java/frc/robot/commands/turret/TurretToHub.java
+++ src/main/java/frc/robot/commands/turret/TurretToHub.java
@@ -28,7 +28,7 @@ public class TurretToHub extends Command {
 
         double robotAngleFromTag9 = hubCenterLocation.minus(swerve.getPose().getTranslation()).getAngle().getDegrees();
 
-        double robotDistanceToTag9 = Math.sqrt(Math.abs(swerve.getPose().getX() - hubCenterLocation.getX()) + Math.abs(swerve.getPose().getY() - hubCenterLocation.getY()));
+        double robotDistanceToTag9 = Math.sqrt(Math.pow(swerve.getPose().getX() - hubCenterLocation.getX(), 2) + Math.pow(swerve.getPose().getY() - hubCenterLocation.getY(), 2));
 
 
         //Calculates the position of the turret on the field by taking the turret's distance and angle (relative to the where the robot's facing) from the center point of the robot
@@ -37,16 +37,34 @@ public class TurretToHub extends Command {
 
         double turretAngleFromTag9 = Math.atan2(hubCenterLocation.getX() - turretPositionX, hubCenterLocation.getY() - turretPositionY);
 
-        double turretDistanceToTag9 = Math.sqrt(Math.abs(turretPositionX - hubCenterLocation.getX()) + Math.abs(turretPositionY - hubCenterLocation.getY()));
+        double turretDistanceToTag9 = Math.sqrt(Math.pow(turretPositionX - hubCenterLocation.getX(), 2) + Math.pow(turretPositionY - hubCenterLocation.getY(), 2));
+
+        /*
+        When we shoot fuel while the robot is moving, the fuel's speed moves relative to the robot's speed, throwing us off.
+        To counteract this, we can calculate the time it will take for the fuel to reach the goal, and project the robot's position at that point. (current position + ( current velocity * the fuel goal time )) 
+        If we point the turret in the direction of the hub based from that position instead of it's current position, it should line up.
+         */
+
+        //Projects the time it will take for the feul to reach the goal when the robot shoots (Currently a shoddy estimate, farther = longer time)
+        double projectedTime = Math.sqrt(robotDistanceToTag9);
+
+        //Projects the turret's projected location relative to the field
+        double projectedTurretPositionX = swerve.getPose().getX() + (swerve.getRobotVelocity().vxMetersPerSecond * projectedTime) + Math.cos(swerve.getPose().getTranslation().getAngle().getDegrees() + turretDegreeFromRobotCenter) * turretDistanceToRobotCenter;
+        double projectedTurretPositionY = swerve.getPose().getY() + (swerve.getRobotVelocity().vyMetersPerSecond * projectedTime) + Math.cos(swerve.getPose().getTranslation().getAngle().getDegrees() + turretDegreeFromRobotCenter) * turretDistanceToRobotCenter;
+
+        //Projects the turret's angle from the hub
+        double projectedTurretAngleFromTag9 = Math.atan2(hubCenterLocation.getX() - projectedTurretPositionX, hubCenterLocation.getY() - projectedTurretPositionY);
+        //Projects the turret's distance to the hub
+        double projectedTurretDistanceToTag9 = Math.sqrt(Math.pow(projectedTurretPositionX - hubCenterLocation.getX(), 2) + Math.pow(projectedTurretPositionY - hubCenterLocation.getY(), 2));
 
         //set turret target position
-        targetTurretPosition = getWrappedAngleDifference(swerve.getPose().getRotation().getDegrees(), turretAngleFromTag9);
+        targetTurretPosition = getWrappedAngleDifference(swerve.getPose().getRotation().getDegrees(), projectedTurretAngleFromTag9);
 
         //set turret hood target position (Rudimentary calculation)
-        if (turretDistanceToTag9 < 1) {
+        if (projectedTurretDistanceToTag9 < 1) {
             targetHoodPosition = 0;
         } else {
-            targetHoodPosition = 30 - (30 / turretDistanceToTag9);
+            targetHoodPosition = 30 - (30 / projectedTurretDistanceToTag9);
         }
     }
 
