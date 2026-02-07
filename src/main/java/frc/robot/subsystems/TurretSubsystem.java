1b547140b14f184c50dd22359b369f52ce336582
ZacharyKingery <zdkingery@gmail.com>
2026-02-07 14:31:05 -0600

    Added a function getShooterPosition to account for the 1.5:1 ratio

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -54,11 +54,11 @@ public TurretSubsystem() {
         turretshooter1config.CurrentLimits.StatorCurrentLimit = 40;
         turretshooter1config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         turretshooter1config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        turretshooter1config.Slot0.kG = 0.455;
-        turretshooter1config.Slot0.kS = 0.245;
-        turretshooter1config.Slot0.kV = 0.131;
-        turretshooter1config.Slot0.kA = 0.0011;
-        turretshooter1config.Slot0.kP = 1.8;
+        turretshooter1config.Slot0.kG = 0.0;
+        turretshooter1config.Slot0.kS = 0.0;
+        turretshooter1config.Slot0.kV = 0.0;
+        turretshooter1config.Slot0.kA = 0.0;
+        turretshooter1config.Slot0.kP = 2;
         turretshooter1config.Slot0.kI = 0.0;
         turretshooter1config.Slot0.kD = 0.0;
         turretshooter1config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
@@ -73,11 +73,11 @@ public TurretSubsystem() {
         turretshooter2config.CurrentLimits.StatorCurrentLimit = 40;
         turretshooter2config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         turretshooter2config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        turretshooter2config.Slot0.kG = 0.455;
-        turretshooter2config.Slot0.kS = 0.245;
-        turretshooter2config.Slot0.kV = 0.131;
-        turretshooter2config.Slot0.kA = 0.0011;
-        turretshooter2config.Slot0.kP = 1.8;
+        turretshooter2config.Slot0.kG = 0.0;
+        turretshooter2config.Slot0.kS = 0.0;
+        turretshooter2config.Slot0.kV = 0.0;
+        turretshooter2config.Slot0.kA = 0.0;
+        turretshooter2config.Slot0.kP = 2;
         turretshooter2config.Slot0.kI = 0.0;
         turretshooter2config.Slot0.kD = 0.0;
         turretshooter2config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
@@ -118,7 +118,11 @@ public TurretSubsystem() {
 //sets speed of the shooter
 public void setShooterSpeed(double power) {
     turretshooter1.set(power);
-    turretshooter2.set(-power);
+    //turretshooter2.set(power);
+}
+
+public double getShooter() {
+    return turretshooter1.getPosition().getValueAsDouble() / 1.5;
 }
 
 //sets rotational speed of the turret
