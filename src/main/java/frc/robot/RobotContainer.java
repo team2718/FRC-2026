b985dff900e5a6045450b43dd9ce22e2923702c9
Noah Zemlin <noah.zemlin@gmail.com>
2026-02-28 23:11:19 -0600

    waiting for shoot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.Constants.RebuiltMatchPeriods;
@@ -162,7 +164,10 @@ public class RobotContainer {
                 new RetractHook(climber));
 
         NamedCommands.registerCommand("TurretToHub",
-                new TurretToHub(turret, swerve, indexer, swerveInput));
+                new ParallelDeadlineGroup(
+                    new WaitCommand(5),
+                    new TurretToHub(turret, swerve, indexer, swerveInput)
+                ));
 
         // Configure button bindings
         configureBindings();
