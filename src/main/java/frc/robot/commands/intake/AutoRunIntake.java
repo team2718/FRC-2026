672318643ab5e00e4f6ba4971477188ef475b6bf
Truett619 <truetth@groundedok.com>
2026-03-26 19:47:33 -0500

    did things

--- src/main/java/frc/robot/commands/intake/AutoRunIntake.java
+++ src/main/java/frc/robot/commands/intake/AutoRunIntake.java
@@ -1,31 +1,31 @@
-package frc.robot.commands.intake;
+// package frc.robot.commands.intake;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.IntakeSubsystem;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.subsystems.IntakeSubsystem;
 
-public class AutoRunIntake extends Command {
-    private final IntakeSubsystem intake;
-    private double speed;
+// public class AutoRunIntake extends Command {
+//     private final IntakeSubsystem intake;
+//     private double speed;
 
-    public AutoRunIntake(IntakeSubsystem intake, double speed) {
-        this.intake = intake;
-        this.speed = speed;
-    }
+//     public AutoRunIntake(IntakeSubsystem intake, double speed) {
+//         this.intake = intake;
+//         this.speed = speed;
+//     }
 
-    @Override
-    public void initialize() {
-        intake.setActive();
-        intake.setIntakeSpeed(speed);
-    }
+//     @Override
+//     public void initialize() {
+//         intake.setActive();
+//         intake.setIntakeSpeed(speed);
+//     }
 
-    @Override
-    public void end(boolean interrupted) {
-        // intake.setIntakeSpeed(0);
-    }
+//     @Override
+//     public void end(boolean interrupted) {
+//         // intake.setIntakeSpeed(0);
+//     }
 
-    @Override
-    public boolean isFinished() {
-        return true;
-    }
+//     @Override
+//     public boolean isFinished() {
+//         return true;
+//     }
 
-}
+// }
